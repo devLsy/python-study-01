f515034f8ac24a4ff4d3421b9f34167ca13205de
이상엽 <infightersy@naver.com>
2025-05-08 12:12:04 +0900

    복습 중

--- 06_if.py
+++ 06_if.py
@@ -2,21 +2,21 @@
 today = "일요일"
 payday = 30
 
-if today == "월요일":
-    print("출근해야지.")
-elif today == "일요일":
-    print("오늘은 쉬는날~!")
-else:
-    print("무슨요일이지?")
+# if today == "월요일":
+#     print("출근해야지.")
+# elif today == "일요일":
+#     print("오늘은 쉬는날~!")
+# else:
+#     print("무슨요일이지?")
 
 print("================================ ")
 
-if payday < 30:
-    print("아직 돈이 안 들어왔음")
-elif payday == 30:
-    print("월급 입금 됨")
-else:
-    print("무슨요일이지?")
+# if payday < 30:
+#     print("아직 돈이 안 들어왔음")
+# elif payday == 30:
+#     print("월급 입금 됨")
+# else:
+#     print("무슨요일이지?")
 
 print("================================ ")
 
@@ -29,9 +29,18 @@ hamburger = False
 
 # if pizza:   # or과 and로 논리 연산처리
 # if pizza or hamburger:   # or 또는 and로 논리 연산처리
-if not pizza:   # 반대로(피자가 True가 아닌 경우)
-    print("피자 또는 햄버거가 있다.")
-elif hamburger:
-    print("햄버거가 있다.")
+# if not pizza:   # 반대로(피자가 True가 아닌 경우)
+#     print("피자 또는 햄버거가 있다.")
+# elif hamburger:
+#     print("햄버거가 있다.")
+# else:
+#     print("아무것도 먹을게 없다.")
+    
+lotto = True
+
+if lotto:
+    print("로또 당첨")
+elif not lotto:
+    print("꽝")
 else:
-    print("아무것도 먹을게 없다.")
+    print("아무것도 없네")
