577f870118888d60604acd2705cb3132110fb157
이상엽 <infightersy@naver.com>
2025-05-08 11:06:41 +0900

    변수, 문자열 포맷팅 테스트

---
+++ 04_var.py
@@ -0,0 +1,12 @@
+# 변수(대소문자 구분)
+name = "thor"
+NAME = "hulk"
+
+# 변수명에 특수문자는 "_" 언더스코어만 사용 가능(공식 스타일 가이드에서 언더스코어 변수 네이밍 권장하고 있음)
+# _name = "under_score"
+# print(type(name))
+
+# print("my name is" + " " + "ironMan")
+print("my name is" + " " + "ironMan")
+print("my name is" + " " + name)
+
