674028d1b0c12be1663899e4cd55379c165999de
이상엽 <infightersy@naver.com>
2025-05-08 11:44:35 +0900

    조건문, 리스트 테스트

---
+++ .idea/07_list.py
@@ -0,0 +1,50 @@
+# 리스트(서로 다른 데이터 타입을 넣을 수도 있음)
+# 리스트 안에 리스트를 넣을 수도 있음
+food0 = ["김치", "삼겹살", "김치", "포도"]
+tmp = ["g", "a", "T", "p"]
+# food0.append("피자")
+print(food0)
+
+# 원하는 인덱스에 추가
+# food0.insert(1, "피자")
+# print(food0)
+
+# 요소 삭제(여러개 있으면 제일 처음 요소만 삭제("김치"가 3개 있을 때 remove를 실행하면 제일 처음의 김치만 삭제됨))
+# food0.remove("김치")
+# print(food0)
+# 인덱스 찾기
+print(food0.count("김치"))
+# 몇번 째 인덱스에 있는지
+print(food0.index("김치"))
+# 오름차순 정렬
+food0.sort()
+print(food0)
+# 내림차순 정렬
+food0.sort(reverse=True)
+print(food0)
+
+print(tmp)
+# 0번 째 인덱스 요소의 값 변경
+tmp[0] = "피자"
+print(tmp)
+
+
+# food1 = [100, "삼겹살", "감자튀김"]
+# food2 =  ["피자", "햄버거"]
+# food3 = food1 + food2
+# food4 = food1 * 3
+
+# print(food1)
+# print(food2)
+# print(food3)
+# print(food4)
+
+
+
+# print(type(food1))
+
+# print(len(food1))
+# print(food1[0])
+# print(food1[0:2])
+# print(type(food1[0]))
+# print(food1[1][0])
---
+++ 06_if.py
@@ -0,0 +1,37 @@
+#조건문
+today = "일요일"
+payday = 30
+
+if today == "월요일":
+    print("출근해야지.")
+elif today == "일요일":
+    print("오늘은 쉬는날~!")
+else:
+    print("무슨요일이지?")
+
+print("================================ ")
+
+if payday < 30:
+    print("아직 돈이 안 들어왔음")
+elif payday == 30:
+    print("월급 입금 됨")
+else:
+    print("무슨요일이지?")
+
+print("================================ ")
+
+# pizza = True
+# 파이썬에서는 아래처럼 비어있는 변수는 False로 인식한다고 함
+# pizza = ""
+# pizza = " " # 이것은 빈 문자열이 있으므로 True로 인식
+pizza = 1 # 숫자는 0이 아닌 경우는 전부 True
+hamburger = False
+
+# if pizza:   # or과 and로 논리 연산처리
+# if pizza or hamburger:   # or 또는 and로 논리 연산처리
+if not pizza:   # 반대로(피자가 True가 아닌 경우)
+    print("피자 또는 햄버거가 있다.")
+elif hamburger:
+    print("햄버거가 있다.")
+else:
+    print("아무것도 먹을게 없다.")
